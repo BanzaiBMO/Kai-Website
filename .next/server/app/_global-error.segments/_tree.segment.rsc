c9650277556fab9adf7cf46adfab9bce8a4f72d4
Kai DeVito <krd55610@uga.edu>
2026-08-17 02:43:22 -0400

    Fix image paths

--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++ .next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"5Fq_5mF1gX675Cg0bi22q"}
+0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"NA8qEc0xu4SZDBktncP2P"}
--- app/projects/electronics/page.js
+++ app/projects/electronics/page.js
@@ -16,21 +16,21 @@ const electronicsItems = [
     },
     {
         loading: "eager",
-        image: require('../../../public/projects-images/electronics-images/Electric Workbench.jpeg'),
+        image: "https://banzaibmo.github.io/Kai-Website/projects-images/electronics-images/Electric%20Workbench.jpeg",
         alt: "Electronic Workbench",
         description:
             "When I took an intensive design course that divided the class into smaller teams, we worked on an electric workbench (from scratch!) that runs from Raspberry Pi OS and implements a voltmeter, ohmmeter, DC reference, and a sinewave + square wave generator. For my team, I worked on the ohmmeter program, the SAR ADC circuit design, and oversought the file structure and code readability. Moreover, I wrote the user manual and the software portion of the technical document. We were one of 13/20 teams that passed all requirements for the project."
     },
     {
         loading: "eager",
-        image: require('../../../public/projects-images/electronics-images/Sensors Robot.jpeg'),
+        image: "https://banzaibmo.github.io/Kai-Website/projects-images/electronics-images/Sensors%20Robot.jpeg",
         alt: "Sensors Robot",
         description:
             "During the summer, I built a robotic interactive pet that uses sensors to respond to the environment, specifically the room brightness, object proximity, and capacitive touch (petting). It responds by changing its emotions to become uncomfortable, scared, or excited from the change in environment. It is also an Internet of Things application because it transfers emotional data via WiFi to be outputted onto a webpage of live data."
     },
     {
         loading: "eager",
-        image: require('../../../public/projects-images/electronics-images/PCB Design.png'),
+        image: "https://banzaibmo.github.io/Kai-Website/projects-images/electronics-images/PCB%20Design.png",
         alt: "Sensors PCB",
         description:
             "Relating to the robotic pet project, I learned how to use KiCad for the first time and achieved a two-layer PCB design if my pet used one instead of breadboards. It uses two ground planes with all componented grounded to them through vias and signal and power connections at different widths to withstand surrounding electromagnetic noise, however it is not an optimal design due to its large surface area; in the future, I will learn to do less common PCB designs such as an 'L' shaped design that would have better suited the pet."
