f111a2a47c0b68e4c5db5159142c3667f025b43b
Kai DeVito <krd55610@uga.edu>
2026-08-17 15:17:10 -0400

    Repair text on contact page and check if resume pdf viewer is good

--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++ .next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"mYDpeh10op7HKKYpBu1K8"}
+0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4272,"slots":null}}},"staleTime":300,"buildId":"QFd85d4zmJWQbXBqUGyLF"}
--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/mYDpeh10op7HKKYpBu1K8/_buildManifest.js",
-    "static/mYDpeh10op7HKKYpBu1K8/_ssgManifest.js",
-    "static/mYDpeh10op7HKKYpBu1K8/_clientMiddlewareManifest.js"
+    "static/QFd85d4zmJWQbXBqUGyLF/_buildManifest.js",
+    "static/QFd85d4zmJWQbXBqUGyLF/_ssgManifest.js",
+    "static/QFd85d4zmJWQbXBqUGyLF/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/22emux2du5j43.js",
